05d80f4e8314c1d66f562aee53e05ee3ea9fd1da
giacomo1235 <giacomo.passariello@gmail.com>
2022-02-21 15:31:56 +0100

    scritto codice, manca arrotondamento seconda cifra dopo lo 0

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+let userAge = prompt('quanti anni hai?');
+let userKm = prompt('quanti Km devi percorrere?');
+
+let price = userKm * 0.21;
+
+if ( userAge < 18) {
+  prezzo = price - price * 0.2;
+}
+else if (userAge > 65) {
+  prezzo = price - price * 0.4;
+}
+else {
+   prezzo = userKm * 0.21;
+}
+
+let element = document.getElementById('htmlOutput');
+
+element.innerHTML = `il prezzo del tuo biglietto è ${prezzo}`;
